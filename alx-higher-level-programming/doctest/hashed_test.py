9627f93e6192860fe59b0967155fecdae234f5d9
Eze Israel John <ezeisrael499@gmail.com>
2024-01-03 23:36:54 +0100

    Added few changes

---
+++ alx-higher-level-programming/doctest/hashed_test.py
@@ -0,0 +1,17 @@
+# keys = ['a', 'aa', 'aaa']
+
+# print('dict:', {k: len(k) for k in keys})
+# print('set :', set(keys))
+
+def double_space(lines):
+    """Prints a list of lines double-spaced.
+
+    >>> double_space(['Line one.', 'Line two.'])
+    Line one.
+    <BLANKLINE>
+    Line two.
+    <BLANKLINE>
+    """
+    for l in lines:
+        print(l)
+        print()
